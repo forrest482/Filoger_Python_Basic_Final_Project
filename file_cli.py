6744e795121d001d808a687099944012fc520a65
Mohammad Ali <forrest482@outlook.com>
2023-11-23 18:50:35 +0330

    add change_directory

--- file_cli.py
+++ file_cli.py
@@ -6,3 +6,13 @@ def list_directory(path='.'):
         return os.listdir(path), None
     except Exception as e:
         return None, str(e)
+
+
+# -----------------------------------------------------
+
+def change_directory(path):
+    try:
+        os.chdir(path)
+        return f"Changed to directory {path}", None
+    except Exception as e:
+        return None, str(e)
